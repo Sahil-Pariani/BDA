4f0ecf45e5eabbb13bc9cbd4417e7c782c498597
Sahil-Pariani <83771542+Sahil-Pariani@users.noreply.github.com>
2021-06-04 23:26:53 +0530

    Add files via upload

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,37 @@
+class HomeController < ApplicationController
+  def dashboard
+  end
+
+  def welcome
+  end
+  
+  def signup
+  end
+
+ def login
+ end
+
+ def about
+ end
+ 
+ def privacy
+ end
+
+ def faq
+ end
+
+def course
+end
+
+def contact
+end
+def cat1
+end
+def cat2
+end
+def cat3
+end
+
+ def terms
+ end 
+end
